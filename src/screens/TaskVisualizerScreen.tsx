1877481ccc760e6d39d16313542af055f4bbca3b
Emmanuel Jimenez <emmsa.jo@hotmail.com>
2025-02-19 17:32:01 -0600

    TaskVisualizerScreen styles refactor

--- src/screens/TaskVisualizerScreen.tsx
+++ src/screens/TaskVisualizerScreen.tsx
@@ -12,49 +12,49 @@ const TaskVisualizerScreen = ({navigation, route}) => {
         <SafeAreaView style={styles.container}>
             <ScrollView style={styles.centerContent} showsVerticalScrollIndicator={false}>
                 
-                <TouchableOpacity style={{marginTop: 20}} onPress={() => navigation.goBack()}>
+                <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
                     <Ionicons name="arrow-back" size={24} color={globalColors.white} />
                 </TouchableOpacity>
-                <Text style={{textAlign: 'center', color: globalColors.white, fontWeight: 'bold', fontSize: 20}}>Task Details</Text>
+                <Text style={styles.title}>Task Details</Text>
 
-                <View style={{backgroundColor: 'white', marginTop: 50, padding: 20, borderRadius: 20}}>    
-                    <View style={{rowGap: 20}}>
-                        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                            <Text style={{color: globalColors.lightGray, fontWeight: 'bold', fontSize: 16}}>Task Title</Text>
+                <View style={styles.cardContainer}>    
+                    <View style={styles.cardGap}>
+                        <View style={styles.cardTitleContainer}>
+                            <Text style={styles.cardTitleText}>Task Title</Text>
                             <TouchableOpacity>
                                 <FontAwesome name="edit" size={26} color={globalColors.secondary} />
                             </TouchableOpacity>
                         </View>
-                        <Text style={{color: globalColors.secondary, fontWeight: 500, fontSize: 25}}>{item.name}</Text>
+                        <Text style={styles.taskName}>{item.name}</Text>
 
-                        <Text style={{color: globalColors.lightGray, fontWeight: 'bold', fontSize: 16}}>Due Date</Text>
-                        <Text style={{color: globalColors.secondary, fontWeight: 500, fontSize: 16}}>{item.date ? new Date(item.date).toDateString() : null}</Text>
+                        <Text style={styles.label}>Due Date</Text>
+                        <Text style={styles.text}>{item.date ? new Date(item.date).toDateString() : null}</Text>
 
 
-                        <Text style={{color: globalColors.lightGray, fontWeight: 'bold', fontSize: 16}}>Status</Text>
+                        <Text style={styles.label}>Status</Text>
                         <View style={{backgroundColor: '#e7f5ef', padding: 5, borderRadius: 10, width: '50%', alignItems: 'center'}}>
                             <Text style={{color: '#0b9b5b', fontSize: 16}}>{item.status == 1 ? 'On Progress' : item.status == 2 ? 'Completed' : null}</Text>
                         </View>
 
-                        <Text style={{color: globalColors.lightGray, fontWeight: 'bold', fontSize: 16}}>Description</Text>
-                        <Text style={{color: globalColors.secondary, fontWeight: 500, fontSize: 16}}>{item.description}</Text>
+                        <Text style={styles.label}>Description</Text>
+                        <Text style={styles.text}>{item.description}</Text>
 
-                        <Text style={{color: globalColors.lightGray, fontWeight: 'bold', fontSize: 16}}>Stages of Task</Text>
-                        <View style={{backgroundColor: '#F9F9FB', padding: 10, borderRadius: 10, flexDirection: 'row', alignItems: 'center', gap: 10}}>
+                        <Text style={styles.label}>Stages of Task</Text>
+                        <View style={styles.stageContainer}>
                             <Ionicons name="checkmark-circle" size={24} color={globalColors.secondary} />
-                            <Text style={{color: globalColors.secondary, fontSize: 16}}>User Research & Analysis</Text>
+                            <Text style={styles.stageText}>User Research & Analysis</Text>
                         </View>
-                        <View style={{backgroundColor: '#F9F9FB', padding: 10, borderRadius: 10, flexDirection: 'row', alignItems: 'center', gap: 10}}>
+                        <View style={styles.stageContainer}>
                             <Ionicons name="checkmark-circle" size={24} color={globalColors.secondary} />
-                            <Text style={{color: globalColors.secondary, fontSize: 16}}>Black & White Wireframe</Text>
+                            <Text style={styles.stageText}>Black & White Wireframe</Text>
                         </View>
-                        <View style={{backgroundColor: '#F9F9FB', padding: 10, borderRadius: 10, flexDirection: 'row', alignItems: 'center', gap: 10}}>
+                        <View style={styles.stageContainer}>
                             <Entypo name="circle" size={22} color={globalColors.secondary} />
-                            <Text style={{color: globalColors.secondary, fontSize: 16}}>Design on Figma</Text>
+                            <Text style={styles.stageText}>Design on Figma</Text>
                         </View>
-                        <View style={{backgroundColor: '#F9F9FB', padding: 10, borderRadius: 10, flexDirection: 'row', alignItems: 'center', gap: 10}}>
+                        <View style={styles.stageContainer}>
                             <Entypo name="circle" size={22} color={globalColors.secondary} />
-                            <Text style={{color: globalColors.secondary, fontSize: 16}}>Presentation on Google Slide</Text>
+                            <Text style={styles.stageText}>Presentation on Google Slide</Text>
                         </View>
                     </View>
                 </View>
@@ -70,9 +70,31 @@ const styles = StyleSheet.create({
             alignItems: 'center',
             paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
         },
-        centerContent: {
-            width: '90%'
-        },
+    centerContent: {
+        width: '90%'
+    },
+    backButton: {
+        marginTop: 20
+    },
+    title: {
+        textAlign: 'center', 
+        color: globalColors.white, 
+        fontWeight: 'bold', fontSize: 20
+    },
+    cardContainer: {
+        backgroundColor: 'white', 
+        marginTop: 50, 
+        padding: 20, 
+        borderRadius: 20
+    },
+    cardGap: {rowGap: 20},
+    cardTitleContainer: {flexDirection: 'row', justifyContent: 'space-between'},
+    cardTitleText: {color: globalColors.lightGray, fontWeight: 'bold', fontSize: 16},
+    taskName: {color: globalColors.secondary, fontWeight: 500, fontSize: 25},
+    label: {color: globalColors.lightGray, fontWeight: 'bold', fontSize: 16},
+    text: {color: globalColors.secondary, fontWeight: 500, fontSize: 16},
+    stageContainer: {backgroundColor: '#F9F9FB', padding: 10, borderRadius: 10, flexDirection: 'row', alignItems: 'center', gap: 10},
+    stageText: {color: globalColors.secondary, fontSize: 16},
 });
 
 export default TaskVisualizerScreen
